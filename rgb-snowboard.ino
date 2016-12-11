a5c760a2359c21978e42df53fe7815d907d218a8
Ferdia McKeogh <ferdiamckeogh@outlook.com>
2016-12-11 14:33:07 +0000

    Added gitignore for .DS_Store files

--- rgb-snowboard.ino
+++ rgb-snowboard.ino
@@ -70,13 +70,13 @@ void updateValues() {
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AccX[index]=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  AccY[index]=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AccZ[index]=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  GyrX[index]=Wire.read()<<8|Wire.read();  // 0x3B (GYRO_XOUT_H) & 0x3C (GYRO_XOUT_L)    
+  GyrY[index]=Wire.read()<<8|Wire.read();  // 0x3D (GYRO_YOUT_H) & 0x3E (GYRO_YOUT_L)
+  GyrZ[index]=Wire.read()<<8|Wire.read();  // 0x3F (GYRO_ZOUT_H) & 0x40 (GYRO_ZOUT_L)
           Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyrX[index]=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyrY[index]=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyrZ[index]=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  AccX[index]=Wire.read()<<8|Wire.read();  // 0x43 (ACCEL_XOUT_H) & 0x44 (ACCEL_XOUT_L)
+  AccY[index]=Wire.read()<<8|Wire.read();  // 0x45 (ACCEL_YOUT_H) & 0x46 (ACCEL_YOUT_L)
+  AccZ[index]=Wire.read()<<8|Wire.read();  // 0x47 (ACCEL_ZOUT_H) & 0x48 (ACCEL_ZOUT_L)
 
   if (index = (ARRAY_SIZE - 1)) {
     index = 0;
