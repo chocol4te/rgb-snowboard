ddf573b71e034755af8acb8b0746be7ae1b702f3
Ferdia McKeogh <ferdiamckeogh@outlook.com>
2016-12-11 14:26:35 +0000

    Finished averaging function

--- rgb-snowboard.ino
+++ rgb-snowboard.ino
@@ -58,10 +58,11 @@ void setup(){
   // Interrupt for bluetooth colour control, also write the android app
 }
 void loop(){
-  updateValues();
+  updateValues(); // Takes approx 2700 microseconds
+  printValues();
   // Calculate new postion and velocity
   // Update LEDs to represent this
-  delay(500);
+  delay(100);
 }
 
 void updateValues() {
@@ -72,17 +73,47 @@ void updateValues() {
   AccX[index]=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
   AccY[index]=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AccZ[index]=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+          Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
   GyrX[index]=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyrY[index]=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyrZ[index]=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  if (index = 4) {
+  if (index = (ARRAY_SIZE - 1)) {
     index = 0;
   }
   else {
     index++;
   }
+
+  for (uint8_t i = 0; i < ARRAY_SIZE; i++) {
+    AvgAccX += AccX[i];
+  }
+  AvgAccX = AvgAccX / ARRAY_SIZE;
+  
+  for (uint8_t i = 0; i < ARRAY_SIZE; i++) {
+    AvgAccY += AccY[i];
+    }
+  AvgAccY = AvgAccY / ARRAY_SIZE;
+  
+  for (uint8_t i = 0; i < ARRAY_SIZE; i++) {
+    AvgAccZ += AccZ[i];
+  }
+  AvgAccZ = AvgAccZ / ARRAY_SIZE;
+  
+  for (uint8_t i = 0; i < ARRAY_SIZE; i++) {
+    AvgGyrX += GyrX[i]; 
+  }
+  AvgGyrX = AvgGyrX / ARRAY_SIZE;
+  
+  for (uint8_t i = 0; i < ARRAY_SIZE; i++) {
+    AvgGyrY += GyrY[i];
+  }
+  AvgGyrY = AvgGyrY / ARRAY_SIZE;
+  
+  for (uint8_t i = 0; i < ARRAY_SIZE; i++) {
+    AvgGyrZ += GyrZ[i];
+  }
+  AvgGyrZ = AvgGyrZ / ARRAY_SIZE;
 }
 
 void printValues() {
